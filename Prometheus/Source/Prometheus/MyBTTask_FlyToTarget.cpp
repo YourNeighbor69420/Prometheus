f70c96d65652127cc63c404226bd6ea4c69f2c3c
YourNeighbor69420 <153528792+YourNeighbor69420@users.noreply.github.com>
2026-03-02 01:38:39 +0000

    Created custom ai fly node

---
+++ Prometheus/Source/Prometheus/MyBTTask_FlyToTarget.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyBTTask_FlyToTarget.h"
+#include "EnemyAIController.h"
+#include "EnemyPawn.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Pawn.h"
+
+UMyBTTask_FlyToTarget::UMyBTTask_FlyToTarget()
+{
+	NodeName = "Fly To Target";
+
+	//keep tick active
+	bNotifyTick = true;
+}
+
+EBTNodeResult::Type UMyBTTask_FlyToTarget::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	//Let the code know to continue running on tick
+	return EBTNodeResult::InProgress;
+}
+
+void UMyBTTask_FlyToTarget::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickTask(OwnerComp, NodeMemory, DeltaSeconds);
+
+	//Get the controller, if not return nothing
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	if (!AIController) return;
+
+	//Get the controllers pawn and blackboard
+	APawn* Pawn = AIController->GetPawn();
+	UBlackboardComponent* BlackboardComponent = OwnerComp.GetBlackboardComponent();
+
+	//If either isn't available, prevent crash
+	if (!BlackboardComponent || !Pawn)
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Failed);
+		return;
+	}
+
+	//Get the selected key in editor and cast it as an actor to be found in world 
+	UObject* TargetObject = BlackboardComponent->GetValueAsObject(GetSelectedBlackboardKey());
+	AActor* TargetActor = Cast<AActor>(TargetObject);
+	
+	FVector PawnLoc = Pawn->GetActorLocation();
+	FVector TargetLoc = TargetActor->GetActorLocation();
+	float Distance = FVector::Dist(PawnLoc, TargetLoc);
+
+	if (Distance <= AcceptableRadius)
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+		return;
+	}
+
+	FVector Direction = (TargetLoc - PawnLoc).GetSafeNormal();
+	Pawn->AddMovementInput(Direction,1.f);
+	
+}
---
+++ Prometheus/Source/Prometheus/MyBTTask_FlyToTarget.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "MyBTTask_FlyToTarget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROMETHEUS_API UMyBTTask_FlyToTarget : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+
+public:
+	UMyBTTask_FlyToTarget();
+
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, Category = "Flight")
+	float AcceptableRadius = 100.f;
+};
