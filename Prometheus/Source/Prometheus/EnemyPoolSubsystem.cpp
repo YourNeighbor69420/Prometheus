b57b7506176f796fa19f5ab6c4a8df8b81e9bc32
YourNeighbor69420 <153528792+YourNeighbor69420@users.noreply.github.com>
2026-03-18 18:59:07 +0000

    Created object pool subsystem

---
+++ Prometheus/Source/Prometheus/EnemyPoolSubsystem.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyPoolSubsystem.h"
+
+#include "EnemyAIController.h"
+
+AEnemyPawn* UEnemyPoolSubsystem::RequestEnemy(TSubclassOf<AEnemyPawn> EnemyClass, const FTransform& SpawnTransform)
+{
+	//Safety Check
+	if (!EnemyClass) return nullptr;
+
+	AEnemyPawn* Enemy = nullptr;
+
+	//If object pool has an array for the enemy and if it isn't empty
+	if (PoolMap.Contains(EnemyClass) && !PoolMap[EnemyClass].InactiveEnemies.IsEmpty())
+	{
+		//If so, Grab the last inactive enemy from the array
+		Enemy = PoolMap[EnemyClass].InactiveEnemies.Pop();
+	}
+	else
+	{
+		//If not, spawn a fresh enemy to be used
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			Enemy = World->SpawnActor<AEnemyPawn>(EnemyClass, SpawnTransform);
+		}
+	}
+	//Prepare the enemy
+	if (Enemy)
+	{
+		//Set it to the spawn point location
+		Enemy -> SetActorTransform(SpawnTransform);
+		//Enemy -> Reactivate();
+	}
+
+	return Enemy;
+}
+
+void UEnemyPoolSubsystem::ReturnEnemy(AEnemyPawn* EnemyToReturn)
+{
+	if (!EnemyToReturn) return;
+
+	//EnemyToReturn->Deactivate();
+	
+	//Find class of enemy
+	TSubclassOf<AEnemyPawn> ClassType = EnemyToReturn->GetClass();
+
+	
+	PoolMap.FindOrAdd(ClassType).InactiveEnemies.Push(EnemyToReturn);
+}
---
+++ Prometheus/Source/Prometheus/EnemyPoolSubsystem.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "EnemyPawn.h"
+#include "EnemyPoolSubsystem.generated.h"
+
+/**
+ * 
+ */
+
+USTRUCT(BlueprintType)
+struct FEnemyPoolArray
+{
+	GENERATED_BODY()
+
+	UPROPERTY(Transient)
+	TArray<AEnemyPawn*> InactiveEnemies;
+	
+};
+UCLASS()
+class PROMETHEUS_API UEnemyPoolSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(Transient)
+	TMap<TSubclassOf<AEnemyPawn>, FEnemyPoolArray> PoolMap;
+
+public:
+
+	UFUNCTION(BlueprintCallable, Category = "Pooling")
+	AEnemyPawn* RequestEnemy(TSubclassOf<AEnemyPawn> EnemyClass, const FTransform& SpawnTransform);
+
+	UFUNCTION(BlueprintCallable, Category = "Pooling")
+	void ReturnEnemy(AEnemyPawn* EnemyToReturn);
+	
+};
