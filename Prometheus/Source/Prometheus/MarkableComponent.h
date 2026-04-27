87a44fae98ab88687555050e3ee8fe40967179c7
YourNeighbor69420 <153528792+YourNeighbor69420@users.noreply.github.com>
2026-04-27 22:44:34 +0100

    Started on level design

--- Prometheus/Source/Prometheus/MarkableComponent.h
+++ Prometheus/Source/Prometheus/MarkableComponent.h
@@ -43,7 +43,7 @@ public:
 
 	void SetExecutableEffectActive(bool bIsEffectActive);
 
-	void Deactivate() override;
+	virtual void Deactivate() override;
 	void Activate();
 	
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Visuals")
